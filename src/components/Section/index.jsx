54db81647da5e7377e0f5a4f5d3e54fcd114f188
nlacerdagabriel <devlacerdagabriel@hotmail.com>
2023-04-08 16:03:14 -0300

    adjusting responsivity mobile

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -2,7 +2,7 @@ import { Flex } from "@chakra-ui/react";
 
 export default function Section(props) {
   return (
-    <Flex overflow="hidden" p="96px 2rem" {...props}>
+    <Flex overflow="hidden" p="96px 1rem" {...props}>
       {props.children}
     </Flex>
   );
